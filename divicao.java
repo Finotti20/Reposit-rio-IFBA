ccff8efc23336156a07a543c5e6d4e0fe65e81b0
Finotti20 <gabrielfinoti41@gmail.com>
2026-04-16 00:24:56 +0000

    Alterações feitas na aula do dia 15/04/2026

---
+++ divicao.java
@@ -0,0 +1,9 @@
+void main(){
+
+    float n1 = Float.parseFloat(IO.readln("Digite o dividendo: "));
+    float n2 = Float.parseFloat(IO.readln("Digite o divisor: "));
+
+    float divisao = n1/n2;
+
+    IO.println("O resultado da divisão é:" + divisao);
+}
---
+++ multiplica.java
@@ -0,0 +1,9 @@
+void main(){
+
+    int n1 = Integer.parseInt(IO.readln("Digite o primeiro número: "));
+    int n2 = Integer.parseInt(IO.readln("Digite o segundo número: "));
+
+    int multi = n1 * n2;
+
+    IO.println("O resultado da multiplicação é: " + multi);
+}
